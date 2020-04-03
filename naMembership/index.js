a2d61e33069b8452c38bd801ad23d5159bb5a7ba
Hussein Taherian <mohammad.hussein.taherian@gmail.com>
2020-04-03 13:57:34 +0430

    create index.js file that is entry for membership module

---
+++ naMembership/index.js
@@ -0,0 +1,4 @@
+const {Authentication} = require('./lib/authentication');
+const {Registration} = require('./lib/registration');
+
+module.exports = Object.assign({}, {Authentication, Registration});
